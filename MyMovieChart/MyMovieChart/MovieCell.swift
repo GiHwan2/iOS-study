c35e7865df65e847fda96956e843dca4858b713b
GiHwan2 <sandwich5011@naver.com>
2022-10-01 19:24:33 +0900

    CH08

---
+++ MyMovieChart/MyMovieChart/MovieCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCell.swift
+//  MyMovieChart
+//
+//  Created by 강기환 on 2022/10/01.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell{
+    
+    @IBOutlet var title: UILabel!
+    @IBOutlet var opendate: UILabel!
+    @IBOutlet var rating: UILabel!
+    @IBOutlet var desc: UILabel!
+    @IBOutlet var thumbnail: UIImageView!
+    
+}
