4ac317f577995eb192205003a4ed380db3f6188b
GiHwan2 <sandwich5011@naver.com>
2022-10-04 18:25:54 +0900

    CH11,12,13

---
+++ MyMovieChart/MyMovieChart/TheaterCell.swift
@@ -0,0 +1,18 @@
+//
+//  TheaterCell.swift
+//  MyMovieChart
+//
+//  Created by 강기환 on 2022/10/04.
+//
+
+import UIKit
+
+class TheaterCell: UITableViewCell{
+    
+    @IBOutlet var name: UILabel!
+    @IBOutlet var tel: UILabel!
+    @IBOutlet var addr: UILabel!
+    
+    
+    
+}
